4001e2740ae9e357014bdf56f50d89b888ee3df9
boparaiamrit <boparaiamrit@gmail.com>
2013-08-09 15:38:41 +0530

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -10,6 +10,6 @@ return array(
 	'redirect' => 'YourSiteURL',
 	//When Someone Logout from your Site
 	'logout' => 'LogoutPageUrl',
-	//you can scope according to your requirement
+	//you can add scope according to your requirement
 	'scope' => 'user_birthday,email,user_hometown,user_location,user_status,user_photos,user_likes,user_education_history'
 	);
